56ef41cbec2394dc793f8a0abbb305b29769222a
Md Mofizul Islam (Sohan) <mofizulislam0216@gmail.com>
2024-01-10 02:26:51 +0600

    Refactor BottomHeaderSec component

--- src/components/Header/BottomHeaderSec/BottomHeaderSec.jsx
+++ src/components/Header/BottomHeaderSec/BottomHeaderSec.jsx
@@ -7,25 +7,29 @@ const BottomHeaderSec = () => {
   return (
     <div className="ns_container">
       <div className="flex items-center gap-5">
-        <div className="border-r border-gray-dark pr-5 flex items-center gap-16">
-          <div className="flex items-center gap-5">
-            <span>
-              <img src={menuSvg} alt="svg" className="h-6 w-6" />
-            </span>
-            <span className="uppercase text-sm font-semibold">
-              Shop categories
-            </span>
-          </div>
-          <div className="mt-0.5">
-            <IoIosArrowDown />
+        <div>
+          <div className="border-r border-gray-dark pr-5 flex items-center gap-16">
+            <div className="flex items-center gap-5">
+              <span>
+                <img src={menuSvg} alt="svg" className="h-6 w-6" />
+              </span>
+              <span className="uppercase text-sm font-semibold">
+                Shop categories
+              </span>
+            </div>
+            <div className="mt-0.5">
+              <IoIosArrowDown />
+            </div>
           </div>
         </div>
-        <div className="flex items-center gap-12">
-          {primaryMenuData.map((item) => (
-            <div key={item.id}>
-              <Link to={item.href}>{item.name}</Link>
-            </div>
-          ))}
+        <div>
+          <div className="flex items-center gap-12">
+            {primaryMenuData.map((item) => (
+              <div key={item.id}>
+                <Link to={item.href}>{item.name}</Link>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
