59570d586696075c83d56df088227741c5cdeec9
Md Mofizul Islam (Sohan) <mofizulislam0216@gmail.com>
2024-01-10 03:01:22 +0600

    Refactor BottomHeaderSec component to use Menu component

--- src/components/Header/BottomHeaderSec/BottomHeaderSec.jsx
+++ src/components/Header/BottomHeaderSec/BottomHeaderSec.jsx
@@ -1,26 +1,50 @@
-import { primaryMenuData } from "../../../assets/data/navbarData";
-import { IoIosArrowDown } from "react-icons/io";
-import menuSvg from "../../../assets/images/menu.svg";
 import { Link } from "react-router-dom";
+import { Menu, MenuHandler, MenuList, Button } from "@material-tailwind/react";
+import { IoIosArrowDown } from "react-icons/io";
+import {
+  categoriesMenuData,
+  primaryMenuData,
+} from "../../../assets/data/navbarData";
+import menuSvg from "../../../assets/images/menu.svg";
+import CategoryItem from "./CategoryItem";
 
 const BottomHeaderSec = () => {
+
   return (
     <div className="ns_container">
       <div className="flex items-center gap-5">
         <div>
-          <div className="border-r border-gray-dark pr-5 flex items-center gap-16">
-            <div className="flex items-center gap-5">
-              <span>
-                <img src={menuSvg} alt="svg" className="h-6 w-6" />
-              </span>
-              <span className="uppercase text-sm font-semibold">
-                Shop categories
-              </span>
-            </div>
-            <div className="mt-0.5">
-              <IoIosArrowDown />
-            </div>
-          </div>
+          <Menu
+            placement="bottom-start"
+            animate={{
+              mount: { y: 0 },
+              unmount: { y: 25 },
+            }}
+            offset={15}
+          >
+            <MenuHandler>
+              <Button className="p-0 text-white" variant="text">
+                <div className="border-r border-gray-dark pr-5 flex items-center gap-16">
+                  <div className="flex items-center gap-5">
+                    <span>
+                      <img src={menuSvg} alt="svg" className="h-6 w-6" />
+                    </span>
+                    <span className="uppercase text-sm font-semibold">
+                      Shop categories
+                    </span>
+                  </div>
+                  <div className="mt-0.5">
+                    <IoIosArrowDown />
+                  </div>
+                </div>
+              </Button>
+            </MenuHandler>
+            <MenuList className="w-64">
+              {categoriesMenuData.map((item) => (
+                <CategoryItem key={item.id} item={item} />
+              ))}
+            </MenuList>
+          </Menu>
         </div>
         <div>
           <div className="flex items-center gap-12">
---
+++ src/components/Header/BottomHeaderSec/CategoryItem.jsx
@@ -0,0 +1,89 @@
+import { forwardRef, useState } from "react";
+import PropTypes from "prop-types";
+import {
+  Menu,
+  MenuHandler,
+  MenuList,
+  MenuItem,
+} from "@material-tailwind/react";
+import { ChevronRightIcon } from "@heroicons/react/24/solid";
+
+const CategoryItem = forwardRef(({ item }, ref) => {
+  const [openMenu, setOpenMenu] = useState(false);
+  const [openSubMenu, setOpenSubMenu] = useState(false);
+
+  return (
+    <div ref={ref}>
+      {item.data && (
+        <Menu
+          placement="right-start"
+          open={openMenu}
+          handler={setOpenMenu}
+          allowHover
+          offset={15}
+        >
+          <MenuHandler className="flex items-center justify-between">
+            <MenuItem>
+              {item.name}
+              <ChevronRightIcon
+                strokeWidth={2.5}
+                className={`h-3.5 w-3.5 transition-transform ${
+                  openMenu ? "rotate-180" : ""
+                }`}
+              />
+            </MenuItem>
+          </MenuHandler>
+          <MenuList>
+            {item.data.map((subItem) => {
+              return (
+                <>
+                  {subItem.data ? (
+                    <>
+                      <Menu
+                        placement="right-start"
+                        open={openSubMenu}
+                        handler={setOpenSubMenu}
+                        allowHover
+                        offset={15}
+                      >
+                        <MenuHandler className="flex items-center justify-between">
+                          <MenuItem>
+                            {subItem.name} {/* Use subItem.name here */}
+                            <ChevronRightIcon
+                              strokeWidth={2.5}
+                              className={`h-3.5 w-3.5 transition-transform ${
+                                openSubMenu ? "rotate-180" : ""
+                              }`}
+                            />
+                          </MenuItem>
+                        </MenuHandler>
+                        <MenuList>
+                          {subItem.data.map((subSubItem) => (
+                            <MenuItem key={subSubItem.id}>
+                              {subSubItem.name}
+                            </MenuItem>
+                          ))}
+                        </MenuList>
+                      </Menu>
+                    </>
+                  ) : (
+                    <MenuItem key={subItem.id}>{subItem.name}</MenuItem>
+                  )}
+                </>
+              );
+            })}
+          </MenuList>
+        </Menu>
+      )}
+      {item.href && <MenuItem>{item.name}</MenuItem>}
+    </div>
+  );
+});
+
+CategoryItem.displayName = "CategoryItem";
+
+CategoryItem.propTypes = {
+  item: PropTypes.object,
+};
+
+export default CategoryItem;
