48d3995f57b9e40c9d7f16a7338bc01f4fb4d24f
Md Mofizul Islam (Sohan) <mofizulislam0216@gmail.com>
2024-01-10 16:54:01 +0600

    Update payment method icons

--- src/assets/data/paymentMethods.js
+++ src/assets/data/paymentMethods.js
@@ -1,52 +1,44 @@
-import visaCard from "../images/payment_methods/Visa.png";
-import masterCard from "../images/payment_methods/Mastercard.png";
-import americanExpress from "../images/payment_methods/Amex.png";
-import paypal from "../images/payment_methods/PayPal.png";
-import shopPay from "../images/payment_methods/ShopPay.png";
-import applePay from "../images/payment_methods/ApplePay.png";
-import googlePay from "../images/payment_methods/GooglePay.png";
-
 export const paymentMethods = [
   {
     id: 1,
     name: "Visa",
     url: "https://www.visa.com",
-    icon: visaCard,
+    icon: "https://res.cloudinary.com/dxzw1lz0v/image/upload/v1704883865/payment_methods/Visa_g1gvce.png",
   },
   {
     id: 2,
     name: "MasterCard",
     url: "https://www.mastercard.com",
-    icon: masterCard,
+    icon: "https://res.cloudinary.com/dxzw1lz0v/image/upload/v1704883865/payment_methods/Mastercard_gblhb8.png",
   },
   {
     id: 3,
     name: "American Express",
     url: "https://www.americanexpress.com",
-    icon: americanExpress,
+    icon: "https://res.cloudinary.com/dxzw1lz0v/image/upload/v1704883865/payment_methods/Amex_rontuv.png",
   },
   {
     id: 4,
     name: "PayPal",
     url: "https://www.paypal.com",
-    icon: paypal,
-  },
-  {
-    id: 5,
-    name: "Shop Pay",
-    url: "https://www.shopify.com/shop-pay",
-    icon: shopPay,
+    icon: "https://res.cloudinary.com/dxzw1lz0v/image/upload/v1704883866/payment_methods/PayPal_atsttx.png",
   },
   {
     id: 6,
     name: "Apple Pay",
     url: "https://www.apple.com/apple-pay",
-    icon: applePay,
+    icon: "https://res.cloudinary.com/dxzw1lz0v/image/upload/v1704883865/payment_methods/ApplePay_tr560w.png",
   },
   {
     id: 7,
     name: "Google Pay",
     url: "https://www.pay.google.com",
-    icon: googlePay,
+    icon: "https://res.cloudinary.com/dxzw1lz0v/image/upload/v1704883865/payment_methods/GooglePay_eouia2.png",
+  },
+  {
+    id: 5,
+    name: "Shop Pay",
+    url: "https://www.shopify.com/shop-pay",
+    icon: "https://res.cloudinary.com/dxzw1lz0v/image/upload/v1704883865/payment_methods/ShopPay_p6u3ca.png",
   },
 ];
