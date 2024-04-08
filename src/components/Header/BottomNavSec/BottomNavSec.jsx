64a59b30c9fe87abaaf7aa950588e106fa88c440
Md Mofizul Islam (Sohan) <mofizulislam0216@gmail.com>
2024-04-08 23:19:37 +0600

    Update BottomNavSec component to fix menu behavior and styling

--- src/components/Header/BottomNavSec/BottomNavSec.jsx
+++ src/components/Header/BottomNavSec/BottomNavSec.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-
 import {
   // Menu,
   // MenuHandler,
@@ -7,7 +6,6 @@ import {
   Button,
 } from "@material-tailwind/react";
 import { IoIosArrowDown } from "react-icons/io";
-// import { ChevronRightIcon } from "@heroicons/react/24/solid";
 import {
   categoriesMenuData,
   primaryMenuData,
@@ -17,49 +15,40 @@ import smoothScrollToTop from "../../Shared/SmoothScroll/SmoothScroll";
 import CategoryItem from "./CategoryItem";
 
 const BottomNavSec = () => {
-    const [openMenu, setOpenMenu] = useState(false);
-    return (
-        <div className="ns_container">
-            <div className="flex items-center gap-5">
-                <div>
-                    <div className="relative">
-                        <div>
-                            <Button onClick={() => setOpenMenu((prev) => !prev)} className="p-0 text-white" variant="text">
-                                <div className="border-r border-gray-dark pr-5 flex items-center gap-16 ">
-                                    <div className="flex items-center gap-5">
-                                        <span>
-                                            <img src={menuSvg} alt="svg" className="h-6 w-6" />
-                                        </span>
-                                        <span className="uppercase text-sm font-semibold">
-                                            Shop categories
-                                        </span>
-                                    </div>
-                                    <div className={`mt-0.5 transition-all duration-300 ease-in-out ${openMenu ? '-rotate-180' : 'rotate-0'}`}>
-                                        <IoIosArrowDown />
-                                    </div>
-
-                                </div>
-                            </Button>
-                        </div>
-                        <div className={`w-full rounded-b-lg absolute top-9 bg-dark-special z-20 overflow-hidden hover:overflow-visible transition-all duration-700 ease-linear ${openMenu ? ' max-h-screen' : 'max-h-0'}`}>
-                            {categoriesMenuData.map((item, index) => (
-                                <CategoryItem key={item.id} item={item} index={index} categoriesMenuData={categoriesMenuData} />
-                            ))}
-                        </div>
-                    </div>
+  const [openMenu, setOpenMenu] = useState(false);
+  return (
+    <div className="ns_container">
+      <div className="flex items-center gap-5">
+        <div>
+          <div className="relative">
+            <div>
+              <Button
+                onClick={() => setOpenMenu((prev) => !prev)}
+                className="p-0 text-white"
+                variant="text"
+              >
+                <div className="border-r border-gray-dark pr-5 flex items-center gap-16 ">
+                  <div className="flex items-center gap-5">
+                    <span>
+                      <img src={menuSvg} alt="svg" className="h-6 w-6" />
+                    </span>
+                    <span className="uppercase text-sm font-semibold">
+                      Shop categories
+                    </span>
+                  </div>
+                  <div
+                    className={`mt-0.5 transition-all duration-300 ease-in-out ${
+                      openMenu ? "-rotate-180" : "rotate-0"
+                    }`}
+                  >
+                    <IoIosArrowDown />
+                  </div>
                 </div>
               </Button>
             </div>
-            {/* <div className="w-full rounded-b-lg absolute top-9 bg-gray-700 z-20">
-              {categoriesMenuData.map((item) => (
-                <div className="py-3 px-4" key={item.id}>
-                  {item.name}
-                </div>
-              ))}
-            </div> */}
             <div
-              className={`w-full rounded-b-lg absolute top-9 bg-dark-special z-20 ${
-                openMenu ? "" : "hidden"
+              className={`w-full rounded-b-lg absolute top-9 bg-dark-special z-20 overflow-hidden hover:overflow-visible transition-all duration-700 ease-linear ${
+                openMenu ? " max-h-screen" : "max-h-0"
               }`}
             >
               {categoriesMenuData.map((item, index) => (
@@ -69,20 +58,23 @@ const BottomNavSec = () => {
                   index={index}
                   categoriesMenuData={categoriesMenuData}
                 />
-                // <div
-                //     className={`py-3 px-4 flex justify-between items-center ${index !== categoriesMenuData.length - 1 && 'border-b border-0.5 border-gray-700'}`}
-                //     key={item.id}
-                // >
-                //     {item.name}
-                //     <ChevronRightIcon
-                //         strokeWidth={2.5}
-                //         className={`h-3.5 w-3.5 transition-transform ${openSubMenu ? "rotate-180" : ""
-                //             }`}
-                //     />
-                // </div>
               ))}
             </div>
           </div>
+        </div>
+        <div
+          className={`w-full rounded-b-lg absolute top-9 bg-dark-special z-20 ${
+            openMenu ? "" : "hidden"
+          }`}
+        >
+          {categoriesMenuData.map((item, index) => (
+            <CategoryItem
+              key={item.id}
+              item={item}
+              index={index}
+              categoriesMenuData={categoriesMenuData}
+            />
+          ))}
         </div>
         <div>
           <div className="flex items-center gap-12">
